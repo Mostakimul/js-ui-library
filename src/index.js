f96d8473234e69bb4bcae07a798b58edfa7e486e
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-08-02 02:43:29 +0200

    dropdown component completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import Tooltip from './app-ui/tooltip';
+import Dropdown from './app-ui/dropdown';
 
 // create tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
-
 tooltip.init();
+
+// create dropdown
+const dropdowns = document.querySelectorAll('.dropdown');
+console.log(dropdowns);
+dropdowns.forEach((dropdown) => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
